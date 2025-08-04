cdc3cf37b186096c53959acc07c64a04bb858548
reza107-hub <rashidaminur104@gmail.com>
2025-08-04 12:35:44 +0600

    till factories

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SingleControllerAction;
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -37,7 +38,11 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     })->name('users');
 });
 
-Route::resource('/blog', BlogController::class);
+// Route::resource('/blog', BlogController::class);
+Route::get('/blog', function () {
+    $blogs = Blog::all();
+    dd($blogs);
+});
 
 
 // fallback route
